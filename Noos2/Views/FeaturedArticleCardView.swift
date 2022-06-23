ef4d91f2d24f204acadabc53668bf2bca4c5a39e
brysonS <brys1saclaus2@gmail.com>
2022-06-22 20:21:54 -0700

    Add article data

--- Noos2/Views/FeaturedArticleCardView.swift
+++ Noos2/Views/FeaturedArticleCardView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct FeaturedArticleCardView: View {
+    
+    let article: Article
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct FeaturedArticleCardView_Previews: PreviewProvider {
+    
+    static var articleSample = ArticleList.articleList[0]
+    
     static var previews: some View {
-        FeaturedArticleCardView()
+        FeaturedArticleCardView(article: articleSample)
     }
 }
